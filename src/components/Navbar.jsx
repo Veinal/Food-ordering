737a91acd6463c67e9d3fae9fe77afcd4a7f9320
VEINAL SEQUEIRA <veinalsequeira5229@gmail.com>
2023-10-13 19:24:53 +0530

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from '@mui/material'
 import { Link } from 'react-router-dom'
+import Logo from '../project-logo.png'
 
 export default function Navbar() {
   return (
@@ -22,8 +23,8 @@ export default function Navbar() {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <a className="navbar-brand mt-2 mt-lg-0" href="#">
                 <img
-                src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-                height="15"
+                src={Logo}
+                height="35"
                 alt="MDB Logo"
                 loading="lazy"
                 />
@@ -45,72 +46,6 @@ export default function Navbar() {
 
                 <Link to='login'><Button variant='contained' >login</Button></Link>
 
-            {/* <a className="text-reset me-3" href="#">
-                <i className="fas fa-shopping-cart"></i>
-            </a> */}
-
-            {/* <div className="dropdown">
-                <a
-                className="text-reset me-3 dropdown-toggle hidden-arrow"
-                href="#"
-                id="navbarDropdownMenuLink"
-                role="button"
-                data-mdb-toggle="dropdown"
-                aria-expanded="false"
-                >
-                <i className="fas fa-bell"></i>
-                <span className="badge rounded-pill badge-notification bg-danger">1</span>
-                </a>
-                <ul
-                className="dropdown-menu dropdown-menu-end"
-                aria-labelledby="navbarDropdownMenuLink"
-                >
-                <li>
-                    <a className="dropdown-item" href="#">Some news</a>
-                </li>
-                <li>
-                    <a className="dropdown-item" href="#">Another news</a>
-                </li>
-                <li>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                </li>
-                </ul>
-            </div> */}
-
-            {/* <div className="dropdown">
-                <a
-                className="dropdown-toggle d-flex align-items-center hidden-arrow"
-                href="#"
-                id="navbarDropdownMenuAvatar"
-                role="button"
-                data-mdb-toggle="dropdown"
-                aria-expanded="false"
-                >
-                <h5>Login</h5>
-
-                <img
-                    src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-                    className="rounded-circle"
-                    height="25"
-                    alt="Black and White Portrait of a Man"
-                    loading="lazy"
-                />
-                </a>
-                <ul
-                className="dropdown-menu dropdown-menu-end"
-                aria-labelledby="navbarDropdownMenuAvatar"
-                >
-                <li>
-                    <a className="dropdown-item" href="#">My profile</a>
-                </li>
-                <li>
-                    <a className="dropdown-item" href="#">Settings</a>
-                </li>
-                <li>
-                    <a className="dropdown-item" href="#">Logout</a>
-                </li>
-                </ul>
-            </div> */}
             </div>
         </div>
         </nav>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState,useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import AppBar from '@mui/material/AppBar';
@@ -14,6 +15,7 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import Navbar from './Navbar';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -24,20 +26,65 @@ import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import { Button } from '@mui/material';
 
+//checkbox 
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
 const drawerWidth = 240;
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function ClippedDrawer() {
+
+  const [get2,setGet2]=useState([])
+  const [search,setSearch] =useState('')
+  const [categories, setCategories] = useState([]);
+
+
+  useEffect(()=>{
+    const set2=JSON.parse(localStorage.getItem("Food"))
+    if(set2){
+      setGet2(set2)
+    }
+  },[])
+
+  // const filteredProducts = get2.filter((item) => {
+  //   const productInfo = `${item.product} ${item.description} ${item.category}`.toLowerCase();
+  //   return (
+  //     productInfo.includes(search.toLowerCase()) &&
+  //     (categories.length === 0 || categories.includes(item.category.toLowerCase()))
+  //   );
+  // });
+
+  const filteredProducts = get2.filter((item) => {
+    const productInfo = `${item.product} ${item.description} ${item.category}`.toLowerCase();
+    return (
+      productInfo.includes(search.toLowerCase()) &&
+      (categories.length === 0 || (item.category && categories.includes(item.category.toLowerCase())))
+    );
+  });
+  
+
+  const handleCategoryChange = (e) => {
+    const categoryName = e.target.name;
+    setCategories((prevCategories) => {
+      if (prevCategories.includes(categoryName)) {
+        // If the category is already selected, remove it
+        return prevCategories.filter((category) => category !== categoryName);
+      } else {
+        // If the category is not selected, add it
+        return [...prevCategories, categoryName];
+      }
+    });
+  };
+  
+
   return (
     <div>
         <Box sx={{ display: 'flex' }}>
           {/* <CssBaseline /> */}
           <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-            {/* <Toolbar>
-              <Typography variant="h6" noWrap component="div">
-                Clipped drawer
-              </Typography>
-            </Toolbar> */}
+            
             <Navbar/>
             
           </AppBar>
@@ -51,72 +98,65 @@ export default function ClippedDrawer() {
           >
             <Toolbar />
             <Box sx={{ overflow: 'auto' }}>
-              <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                  <ListItem key={text} disablePadding>
-                    <ListItemButton>
-                      <ListItemIcon>
-                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                      </ListItemIcon>
-                      <ListItemText primary={text} />
-                    </ListItemButton>
-                  </ListItem>
-                ))}
-              </List>
-              <Divider />
-              <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                  <ListItem key={text} disablePadding>
-                    <ListItemButton>
-                      <ListItemIcon>
-                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                      </ListItemIcon>
-                      <ListItemText primary={text} />
-                    </ListItemButton>
-                  </ListItem>
-                ))}
-              </List>
+              <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+                <FormGroup>
+                  {/* <FormControlLabel control={<Checkbox defaultChecked />} label="Label" /> */}
+                  <Typography variant='h4'><b>Categories:</b></Typography>
+                  <FormControlLabel control={<Checkbox checked={categories.includes('category1')} onChange={handleCategoryChange} name="category1"/>} label="category1" />
+                  <FormControlLabel control={<Checkbox checked={categories.includes('category2')} onChange={handleCategoryChange} name="category2"/>} label="category2" />
+                  <FormControlLabel control={<Checkbox checked={categories.includes('category3')} onChange={handleCategoryChange} name="category3"/>} label="category3" />
+                  <FormControlLabel control={<Checkbox checked={categories.includes('category4')} onChange={handleCategoryChange} name="category4"/>} label="category4" />
+                  <FormControlLabel control={<Checkbox checked={categories.includes('category5')} onChange={handleCategoryChange} name="category5"/>} label="category5" />
+                  <FormControlLabel control={<Checkbox checked={categories.includes('category6')} onChange={handleCategoryChange} name="category6"/>} label="category6" />
+                  
+                </FormGroup>
+              </div>
+
             </Box>
           </Drawer>
           <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
             <Toolbar />
-
-            <div>
-              <Container sx={{ py: 8 }} maxWidth="md">
-                    {/* End hero unit */}
-                    <Grid container spacing={4}>
-                      {cards.map((card) => (
-                        <Grid item key={card} xs={12} sm={6} md={4}>
-                          <Card
-                            sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                          >
-                            <CardMedia
-                              component="div"
-                              sx={{
-                                // 16:9
-                                pt: '56.25%',
-                              }}
-                              image="https://source.unsplash.com/random?wallpapers"
-                            />
-                            <CardContent sx={{ flexGrow: 1 }}>
-                              <Typography gutterBottom variant="h5" component="h2">
-                                Heading
-                              </Typography>
-                              <Typography>
-                                This is a media card. You can use this section to describe the
-                                content.
-                              </Typography>
-                            </CardContent>
-                            <CardActions>
-                              <Button size="small">View</Button>
-                              <Button size="small">Edit</Button>
-                            </CardActions>
-                          </Card>
-                        </Grid>
-                      ))}
-                    </Grid>
-                  </Container>
+            {/* <input type="search" name="" id="" placeholder='search'  /><SearchOutlinedIcon/> */}
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <div className="input-group" style={{width:"50%"}}>
+                <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" value={search} onChange={(e)=>setSearch(e.target.value)} />
+                <button type="button" className="btn btn-primary">search</button>
               </div>
+            </div>
+
+            <Container sx={{ py: 3 }}>
+              <div style={{display: 'grid',gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',gap: '1rem'}}>
+              {filteredProducts?.map((item)=>{
+                return(
+                  <>
+                    <Card sx={{ maxWidth: 345 }} key={item.product}>
+                      <CardMedia
+                        component="img"
+                        alt="product"
+                        height="140"
+                        image={item.image}
+                      />
+                      <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+                          <h3>{item.product}</h3>
+                          <h3>{item.price}</h3>
+                          <h3>{item.status}</h3>
+                          <h3>{item.category}</h3>
+                          <h3>{item.description}</h3>
+                        </Typography>
+                      </CardContent>
+  
+                      <CardActions>
+                        <Button size="small">View</Button>
+                        <Button size="small">Order</Button>
+                      </CardActions>
+                    </Card>
+                  </>
+                )
+              })}
+              
+              </div>
+            </Container>
             
           </Box>
         </Box>
